6088f9e9fc5370c31cf3a3ab4dc0e8248845c9bf
MarioDena <enrique.dn@outlook.com>
2020-05-19 22:06:36 -0500

    Added basic game structure

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,17 @@
+import 'phaser'; 
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+ 
+  preload () {
+    // load images
+    this.load.image('logo', './src/assets/title.jpeg');
+  }
+ 
+  create () {
+    this.add.image(400, 300, 'logo');
+  }
+};
+
+
